7bff191fb84931325cf1c18fa19cb8c0ee380681
Giulia Correa <124733742+GiuliaCorrea100@users.noreply.github.com>
2024-08-16 17:00:46 -0400

    Add files via upload

---
+++ 99BottlesChallenge.js
@@ -0,0 +1,7 @@
+var bottlesBeer = 99;
+
+while (bottlesBeer >= 1) {
+    console.log(bottlesBeer + " bottles of beer on the wall, " + bottlesBeer + " bottles of beer. ");
+    bottlesBeer --;
+    console.log("Take one down and pass it around, " + bottlesBeer + " bottles of beer on the wall.");
+}
